2906991126c7e15666a0926176bf320006bd2502
Issac Kurian <issackurian1@gmail.com>
2022-01-29 18:42:37 -0500

    Common func. to count increases

--- src/main/scala/day1/Main.scala
+++ src/main/scala/day1/Main.scala
@@ -5,23 +5,24 @@ import scala.io.Source
 val inFile = "/home/issac/Learn/aoc2021/src/main/resources/day1/input.txt"
 val inputLines = Source.fromFile(inFile).getLines.map(_.toInt).toList
 
+def countIncreases[T](sumFunc: T => Int)(items: List[T]): Int = {
+  val (numIncreases, _) = items.foldLeft[(Int, Option[T])](0, None) {
+    case ((numInx, None), curr) => (numInx, Option(curr))
+    case ((numInx, Some(prev)), curr) =>
+      val prevSum = sumFunc(prev)
+      val currSum = sumFunc(curr)
+      if (currSum > prevSum) (numInx + 1, Option(curr)) else (numInx, Option(curr))
+  }
+  numIncreases
+}
+
 @main def part1: Unit = 
   println(s"Number of measurements: ${inputLines.size}")
-  val (numIncreases, _) = inputLines.foldLeft[(Int, Option[Int])]((0, None)) {
-    case ((numInx, None), curr) =>
-      (numInx, Some(curr))
-    case ((numInx, Some(prev)), curr) => if (curr > prev) (numInx + 1, Some(curr)) else (numInx, Some(curr))
-  }
+  val numIncreases = countIncreases[Int](identity)(inputLines)
   println(s"Number of increases: $numIncreases")
 
 @main def part2: Unit =
   println(s"Number of measurements: ${inputLines.size}")
   val grped = inputLines.sliding(3).toList
-  val (numIncreases, _) = grped.foldLeft[(Int, List[Int])]((0, Nil)) {
-    case ((numInx, Nil), curr) => (numInx, curr)
-    case ((numInx, prev), curr) =>
-      val prevSum = prev.sum
-      val currSum = curr.sum
-      if (currSum > prevSum) (numInx + 1, curr) else (numInx, curr)
-  }
+  val numIncreases = countIncreases[List[Int]](_.sum)(grped)
   println(s"Number of increases: $numIncreases")
