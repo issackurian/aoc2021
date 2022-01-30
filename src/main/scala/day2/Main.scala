fedb098353673886785c40cbb4636f175099e4c8
Issac Kurian <issackurian1@gmail.com>
2022-01-29 22:57:14 -0500

    Day2 Pt1

---
+++ src/main/scala/day2/Main.scala
@@ -0,0 +1,21 @@
+package day2
+
+import scala.io.Source
+
+val inFile = "/home/issac/Learn/aoc2021/src/main/resources/day2/input.txt"
+val inputLines = Source.fromFile(inFile).getLines.toList
+
+case class Coordinates(x: Int, y: Int)
+
+@main def part1: Unit =
+  val start = Coordinates(0, 0)
+  println(s"Number of moves: ${inputLines.size}")
+  val end = inputLines.foldLeft(start) { (pos, move) =>
+    move.split(" ") match {
+      case Array("forward", numSpaces) => pos.copy(x = pos.x + numSpaces.toInt)
+      case Array("down", numSpaces) => pos.copy(y = pos.y + numSpaces.toInt)
+      case Array("up", numSpaces) => pos.copy(y = pos.y - numSpaces.toInt)
+    }
+  }
+
+  println(s"End: $end - area: ${end.x * end.y}")
